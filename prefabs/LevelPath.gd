808ba2c698c17ebfa689f045d3456a035f7efde1
GabrielCancro3OGS <119443886+GabrielCancro3OGS@users.noreply.github.com>
2025-01-26 10:29:41 -0300

    update

--- prefabs/LevelPath.gd
+++ prefabs/LevelPath.gd
@@ -1,30 +1,38 @@
 extends Node2D
 tool
 
-var myId = -1
-export var nodeNeedId = -1
 enum TypeEnum {LEVEL, SUN, CHEST}
 export(TypeEnum) var type
 
-func _ready():
-	pass # Replace with function body.
+var close_nodes
+
+func _ready(): pass
 
 func _process(delta):
-	var node = get_node_or_null("../P"+str(nodeNeedId))
-	var myId = get_index()+1
 	if Engine.editor_hint:
 		$N/sun.rect_rotation += 4 * PI * delta
-		if !node: $Line2D.points[1] = $Line2D.points[0]
-		else: $Line2D.points[1] = (node as Node2D).global_position - global_position
-		$N/lbl_num.text = str(myId)
-		$N/sun.visible = false
-		$N/level.visible = false
-		$N/chest.visible = false
-		if type == TypeEnum.LEVEL: $N/level.visible = true
-		if type == TypeEnum.SUN: $N/sun.visible = true
-		if type == TypeEnum.CHEST: $N/chest.visible = true
-#		$Line2D.points = [(Vector2(0, 0)]
-#		for u in get_parent().get_children():
-			
-			
-	
+		set_type()
+		set_close_nodes()
+		connect_close_nodes()
+
+func set_type():
+	$N/lbl_num.text = str(get_index()+1)
+	$N/sun.visible = false
+	$N/level.visible = false
+	$N/chest.visible = false
+	if type == TypeEnum.LEVEL: $N/level.visible = true
+	if type == TypeEnum.SUN: $N/sun.visible = true
+	if type == TypeEnum.CHEST: $N/chest.visible = true
+
+func set_close_nodes():
+	close_nodes = []
+	for lp in get_parent().get_children():
+		var dist = position.distance_to(lp.position)
+		if dist>20 && dist<100: close_nodes.append(lp)
+
+func connect_close_nodes():
+	for ln in $L.get_children():
+		var i = ln.get_index()
+		if i < close_nodes.size():
+			ln.points[1] = close_nodes[i].global_position - global_position
+		else: ln.points[1] = Vector2(0,9)
