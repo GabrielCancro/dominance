e122d8b9e1e0b958488647a2575cca46f4dbf02a
GabrielCancro3OGS <119443886+GabrielCancro3OGS@users.noreply.github.com>
2025-01-25 23:46:41 -0300

    update

--- prefabs/LevelPath.gd
+++ prefabs/LevelPath.gd
@@ -3,6 +3,8 @@ tool
 
 var myId = -1
 export var nodeNeedId = -1
+enum TypeEnum {LEVEL, SUN, CHEST}
+export(TypeEnum) var type
 
 func _ready():
 	pass # Replace with function body.
@@ -12,8 +14,14 @@ func _process(delta):
 	var myId = get_index()+1
 	if Engine.editor_hint:
 		$N/sun.rect_rotation += 4 * PI * delta
-		$N/sun.visible = false
 		if !node: $Line2D.points[1] = $Line2D.points[0]
 		else: $Line2D.points[1] = (node as Node2D).global_position - global_position
 		$N/lbl_num.text = str(myId)
+		$N/sun.visible = false
+		$N/level.visible = false
+		$N/chest.visible = false
+		if type == TypeEnum.LEVEL: $N/level.visible = true
+		if type == TypeEnum.SUN: $N/sun.visible = true
+		if type == TypeEnum.CHEST: $N/chest.visible = true
+			
 	
