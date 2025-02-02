648ac126bebe1645ae25463b59168c882364281a
GabrielCancro3OGS <119443886+GabrielCancro3OGS@users.noreply.github.com>
2025-02-02 19:58:10 -0300

    update

--- prefabs/effects/fx_sunpoint_getted.gd
+++ prefabs/effects/fx_sunpoint_getted.gd
@@ -1,9 +1,8 @@
 extends TextureRect
 
 func _ready():
-	$Tween.interpolate_property(self,"modulate:a",0,.5,1,Tween.TRANS_QUAD,Tween.EASE_IN_OUT,0)
-	$Tween.interpolate_property(self,"modulate:a",.5,0,1,Tween.TRANS_QUAD,Tween.EASE_IN_OUT,2)
-	#$Tween.interpolate_property(self,"rect_global_position",start_position,to_pos,3,Tween.TRANS_LINEAR,Tween.EASE_IN_OUT,0)
-	$Tween.interpolate_property(self,"rect_scale",Vector2(1,1),Vector2(1.2,1.2),3,Tween.TRANS_LINEAR,Tween.EASE_IN_OUT,0)
+	$Tween.interpolate_property(self,"modulate:a",1,0,.7,Tween.TRANS_QUAD,Tween.EASE_OUT,0)
+	$Tween.interpolate_property(self,"rect_global_position",null,Vector2(0,0),.8,Tween.TRANS_LINEAR,Tween.EASE_OUT,0)
+	$Tween.interpolate_property(self,"rect_scale",Vector2(1.2,1.2),Vector2(1,1),.7,Tween.TRANS_LINEAR,Tween.EASE_OUT,0)
 	$Tween.start()
 	#yield($Tween,"tween_all_completed")
